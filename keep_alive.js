d81269949e0eb045f2ce2d858df5eaf4f284f895
1w1nters <nikitazolotaryov2711@gmail.com>
2025-12-09 17:56:39 +0200

    Update keep_alive.js

--- keep_alive.js
+++ keep_alive.js
@@ -3,11 +3,14 @@ import express from 'express';
 const server = express();
 
 server.all('/', (req, res) => {
-  res.send('Bot is alive. Grinding 24/7.');
+  res.send('Bot is alive! 🎅');
 });
 
 export function keepAlive() {
-  server.listen(3000, () => {
-    console.log('Server is ready.');
+  // Render выдает порт в process.env.PORT. Если его нет, используем 3000.
+  const port = process.env.PORT || 3000;
+  
+  server.listen(port, '0.0.0.0', () => {
+    console.log(`Server is ready on port ${port}`);
   });
 }
